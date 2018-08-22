110cc4bc0b3810ce33f1968643d5b7b960a3d5de
victor gichohi <victorgichohi48@gmail.com>
2018-08-22 05:06:12 +0300

    email integration

--- accounts/models.py
+++ accounts/models.py
@@ -80,6 +80,118 @@ class User(AbstractBaseUser):
 	def is_admin(self):
 		return self.admin
 
-		
+class EmailActivationQuerySet(models.query.QuerySet):
+    def confirmable(self):
+        now = timezone.now()
+        start_range = now - timedelta(days=DEFAULT_ACTIVATION_DAYS)
+        # does my object have a timestamp in here
+        end_range = now
+        return self.filter(
+                activated = False,
+                forced_expired = False
+              ).filter(
+                timestamp__gt=start_range,
+                timestamp__lte=end_range
+              )
+
+
+class EmailActivationManager(models.Manager):
+    def get_queryset(self):
+        return EmailActivationQuerySet(self.model, using=self._db)
+
+    def confirmable(self):
+        return self.get_queryset().confirmable()
+
+    def email_exists(self, email):
+        return self.get_queryset().filter(
+                    Q(email=email) | 
+                    Q(user__email=email)
+                ).filter(
+                    activated=False
+                )
+
+
+class EmailActivation(models.Model):
+    user            = models.ForeignKey(User)
+    email           = models.EmailField()
+    key             = models.CharField(max_length=120, blank=True, null=True)
+    activated       = models.BooleanField(default=False)
+    forced_expired  = models.BooleanField(default=False)
+    expires         = models.IntegerField(default=7) # 7 Days
+    timestamp       = models.DateTimeField(auto_now_add=True)
+    update          = models.DateTimeField(auto_now=True)
+
+    objects = EmailActivationManager()
+
+    def __str__(self):
+        return self.email
+
+    def can_activate(self):
+        qs = EmailActivation.objects.filter(pk=self.pk).confirmable() # 1 object
+        if qs.exists():
+            return True
+        return False
+
+    def activate(self):
+        if self.can_activate():
+            # pre activation user signal
+            user = self.user
+            user.is_active = True
+            user.save()
+            # post activation signal for user
+            self.activated = True
+            self.save()
+            return True
+        return False
+
+    def regenerate(self):
+        self.key = None
+        self.save()
+        if self.key is not None:
+            return True
+        return False
+
+    def send_activation(self):
+        if not self.activated and not self.forced_expired:
+            if self.key:
+                base_url = getattr(settings, 'BASE_URL', 'https://www.pythonecommerce.com')
+                key_path = reverse("account:email-activate", kwargs={'key': self.key}) # use reverse
+                path = "{base}{path}".format(base=base_url, path=key_path)
+                context = {
+                    'path': path,
+                    'email': self.email
+                }
+                txt_ = get_template("registration/emails/verify.txt").render(context)
+                html_ = get_template("registration/emails/verify.html").render(context)
+                subject = '1-Click Email Verification'
+                from_email = settings.DEFAULT_FROM_EMAIL
+                recipient_list = [self.email]
+                sent_mail = send_mail(
+                            subject,
+                            txt_,
+                            from_email,
+                            recipient_list,
+                            html_message=html_,
+                            fail_silently=False,
+                    )
+                return sent_mail
+        return False
+
+
+
+def pre_save_email_activation(sender, instance, *args, **kwargs):
+    if not instance.activated and not instance.forced_expired:
+        if not instance.key:
+            instance.key = unique_key_generator(instance)
+
+pre_save.connect(pre_save_email_activation, sender=EmailActivation)
+
+
+def post_save_user_create_reciever(sender, instance, created, *args, **kwargs):
+    if created:
+        obj = EmailActivation.objects.create(user=instance, email=instance.email)
+        obj.send_activation()
+
+post_save.connect(post_save_user_create_reciever, sender=User)
 
   
